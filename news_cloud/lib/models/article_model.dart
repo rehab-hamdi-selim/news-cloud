13bc2d6bea3977337c6a966d2595098e5443ecda
Rehab Hamdy <209379697+rehab-hamdi-selim@users.noreply.github.com>
2025-05-01 16:29:29 +0300

    models

---
+++ news_cloud/lib/models/article_model.dart
@@ -0,0 +1,11 @@
+class ArticleModel {
+  String? title;
+  String? description;
+  String? image;
+
+  ArticleModel({
+    required this.title,
+    required this.description,
+    required this.image,
+  });
+}
