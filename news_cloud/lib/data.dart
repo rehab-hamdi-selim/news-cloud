8d458f1c73f15e64bd753ef5aacb84bc170a7aea
Rehab Hamdy <209379697+rehab-hamdi-selim@users.noreply.github.com>
2025-05-01 16:30:15 +0300

    data file

---
+++ news_cloud/lib/data.dart
@@ -0,0 +1,13 @@
+
+import 'models/category_model.dart';
+
+List<CategoryModel> categories = [
+  CategoryModel(image: 'assets/images/science.jpeg', name: 'Science'),
+  CategoryModel(image: 'assets/images/sport.jpeg', name: 'Sports'),
+  CategoryModel(image: 'assets/images/technology.jpeg', name: 'Technology'),
+  CategoryModel(image: 'assets/images/business.jpeg', name: 'Business'),
+  CategoryModel(image: 'assets/images/general.jpeg', name: 'General'),
+  CategoryModel(image: 'assets/images/health.jpeg', name: 'Health'),
+  CategoryModel(
+      image: 'assets/images/entertainment.jpeg', name: 'Entertainment'),
+];
